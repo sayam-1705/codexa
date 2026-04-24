975e6e91450e6a2046bc7534e3adf8c6d5e65c51
sayam-1705 <sayam2022mondal@gmail.com>
2026-04-24 16:37:43 +0530

    fixed npm urls

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://https://codexa-toolkit.vercel.app',
+  site: 'https://codexa-toolkit.vercel.app',
   output: 'static',
   build: {
     format: 'directory'

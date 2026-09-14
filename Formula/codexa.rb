abe98a5eee35a5c6b76a4592217ec3da078192d7
Sayam Mondal <sayam2022mondal@gmail.com>
2026-09-15 04:15:04 +0530

    Update Homebrew formula for 1.1.3

--- Formula/codexa.rb
+++ Formula/codexa.rb
@@ -1,8 +1,8 @@
 class Codexa < Formula
   desc "Blame-aware pre-commit guardian with verified auto-fix"
   homepage "https://codexa-toolkit.vercel.app"
-  url "https://github.com/sayam-1705/codexa/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "49b4e177059490eb143dfcb3de8850edd1c7fe86a3f25e3e66f0f3cfae932977"
+  url "https://github.com/sayam-1705/codexa/archive/refs/tags/v1.1.3.tar.gz"
+  sha256 "9f399e54c881a775e367d4f460b076f95c8dbe4485e0c708477da8bf498be41e"
   license "MIT"
 
   depends_on "node"
@@ -17,6 +17,6 @@ class Codexa < Formula
   end
 
   test do
-    assert_match "1.1.1", shell_output("#{bin}/codexa --version")
+    assert_match "1.1.3", shell_output("#{bin}/codexa --version")
   end
 end
